8964a585d1b48b1f944c26513e97eae3b4405236
Patrik Trefil <patrik.trefil@gmail.com>
2022-10-13 10:45:39 +0200

    use sftp to share video and logs

--- dashcam.sh
+++ dashcam.sh
@@ -2,8 +2,10 @@
 
 set -euo pipefail
 
-LOG_FILE="${HOME}/Logs/dashcam.log"
-VIDEO_DIR="${HOME}/Videos/dashcam/"
+# let's save everything to DATA_DIR (in my specific case that folder is shared via sftp)
+DATA_DIR="/data/pi/upload/"
+LOG_FILE="${DATA_DIR}/Logs/dashcam.log"
+VIDEO_DIR="${DATA_DIR}/Videos/dashcam/"
 
 when="$(date '+%Y-%m-%d--%H-%M')"
 
@@ -14,4 +16,5 @@ touch "$LOG_FILE"
 echo "Started at: $when" | tee "$LOG_FILE" 1>/dev/null
 
 # start recording; save to VIDEO_DIR and include timestamp in filename
-ffmpeg -i /dev/video0 "${VIDEO_DIR}${when}.mp4"
+mkdir -p "$VIDEO_DIR"
+ffmpeg -i /dev/video0 "${VIDEO_DIR}${when}.mp4" &
