292cbe1d00486604c291bde37447ed8ed1217feb
Patrik Trefil <patrik.trefil@gmail.com>
2022-10-13 17:19:30 +0200

    reorganize start and stop of service scripts

--- cut_current_video_recording_and_delete_old_videos.sh
+++ cut_current_video_recording_and_delete_old_videos.sh
@@ -3,7 +3,8 @@
 set -euo pipefail
 
 # remove files older than 3 days
-find /home/pi/Videos/dashcam/ -type f -iname '*.mp4' -mtime +3 -exec rm {} \;
+find /data/pi/upload/Videos/dashcam/ -type f -iname '*.mp4' -mtime +3 -exec rm {} \;
 
 # start dashcam service
+sudo systemctl stop dashcam
 sudo systemctl start dashcam
--- stopdashcam.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-sudo systemctl stop dashcam.service
